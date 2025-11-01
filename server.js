ff2e0afd7f3db36edbefa7dff775cfcd06cd7630
lara <laracanada2005@gmail.com>
2025-10-31 23:40:13 -0400

    Added a server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => res.send("Notes API Running"));
+
+app.listen(process.env.PORT, () => console.log(`🚀 Server on port ${process.env.PORT}`));
